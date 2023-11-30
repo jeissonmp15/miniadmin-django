3737d78bf46b0eddff0f65d192373396dc6777ee
Jeisson Felipe Manrique Pulido <jeissonmp15@gmail.com>
2023-11-30 18:28:46 -0500

    Finaliza CRUD Clientes

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,8 +1,15 @@
 
-from django.urls import path, include
+from django.urls import path
 
-from .views import get
+from .views import get, retrieve, update, post, delete
+
+app_name = 'clientes'
 
 urlpatterns = [
-    path('', get, name='get')
+    path('', get, name='get'),
+    path('crear/', post, name='create'),
+    path('<int:cliente_id>/', update, name='update'),
+    path('<int:cliente_id>/detalle/', retrieve, name='detail'),
+    path('<int:cliente_id>/eliminar/', delete, name='delete'),
+    
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from clientes.models import Clientes
@@ -8,3 +8,43 @@ def get(request):
     queryset = Clientes.objects.all()
     context = {'lista_clientes': queryset}
     return render(request, 'clientes/clientes.html', context)
+
+
+def retrieve(request, cliente_id):
+    cliente = get_object_or_404(Clientes, pk=cliente_id)
+    return render(request, 'clientes/cliente-detalle.html', {'instancia': cliente})
+
+
+def update(request, cliente_id):
+    cliente = get_object_or_404(Clientes, pk=cliente_id)
+    if request.method == 'POST':
+        cliente.nombre = request.POST['nombre']
+        cliente.nit = request.POST['nit']
+        cliente.nombre_contacto = request.POST['nombre_contacto']
+        cliente.telefono = request.POST['tel']
+        cliente.direccion = request.POST['direccion']
+        cliente.correo = request.POST['correo']
+
+        cliente.save()
+
+    return render(request, 'clientes/cliente-actualizar.html', {'instancia': cliente})
+
+
+def post(request):
+    if request.method == 'POST':
+        Clientes.objects.create(
+            nombre=request.POST['nombre'],
+            nit=request.POST['nit'],
+            nombre_contacto=request.POST['nombre_contacto'],
+            telefono=request.POST['tel'],
+            direccion=request.POST['direccion'],
+            correo=request.POST['correo']
+        )
+        
+    return render(request, 'clientes/cliente-crear.html') 
+
+
+def delete(request, cliente_id):
+    cliente = get_object_or_404(Clientes, pk=cliente_id)
+    cliente.delete()
+    return get(request)
